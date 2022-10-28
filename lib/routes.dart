ecee4b7a1f3d5e7f548bef71854a2fec0bf7a29a
fortuna <avidia38@gmail.com>
2022-10-28 18:45:49 +0900

    refactor: ElevatedButton

--- lib/routes.dart
+++ lib/routes.dart
@@ -10,35 +10,89 @@ class Routes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder:(context) => BottomBar()),
-            );
-          }, child: Text('Google Nav Bar'),),
-          ElevatedButton(onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder:(context) => ChoiceChipSelector()),
-            );
-          }, child: Text('Choice Chip Tip'),),
-          ElevatedButton(onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder:(context) => ImageSlider()),
-            );
-          }, child: Text('Image Slider'),),
-          ElevatedButton(onPressed: (){}, child: Text(''),),
-          ElevatedButton(onPressed: (){}, child: Text(''),),
-          ElevatedButton(onPressed: (){}, child: Text(''),),
-          ElevatedButton(onPressed: (){}, child: Text(''),),
-          ElevatedButton(onPressed: (){}, child: Text(''),),
-        ],
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: _boxDecoration(),
+              child: _button(context, BottomBar(), 'BottomBar'),
+            ),
+            const SizedBox(height: 15),
+            Container(
+              decoration: _boxDecoration(),
+              child: _button(
+                context,
+                ChoiceChipSelector(),
+                'ChoiceChipSelector',
+              ),
+            ),
+            Container(
+              decoration: _boxDecoration(),
+              child: _button(
+                context,
+                ImageSlider(),
+                ' ImageSlider',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(''),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(''),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(''),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(''),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(''),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  BoxDecoration _boxDecoration() {
+    return BoxDecoration(
+      color: Colors.grey.shade300,
+      borderRadius: BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          spreadRadius: 3,
+          color: Colors.grey.shade700,
+          blurRadius: 10,
+          offset: const Offset(5, 5),
+        ),
+        const BoxShadow(
+          spreadRadius: 3,
+          color: Colors.white,
+          blurRadius: 12,
+          offset: Offset(-5, -5),
+        ),
+      ],
+    );
+  }
+
+  _button(BuildContext context, Widget widget, String text) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => widget),
+        );
+      },
+      child: Text(text),
     );
   }
 }
